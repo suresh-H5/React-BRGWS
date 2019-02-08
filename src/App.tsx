09ddee10871ee9836c7c81b9f020a2a2c72c6430
suresh <v-surdan@microsoft.com>
2019-02-08 18:25:12 +0530

    Typescript has been added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
@@ -11,12 +13,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
---
+++ src/Components/Home/home.component.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am home component.</h1>
+      </div>
+    );
+  }
+}
